9be570c7c2040c94a0a625f214406261262f5523
Petter Lauvrak <petter.lauvrak@hotmail.com>
2025-02-09 14:25:17 +0100

    fix build

--- src/lib/authUtils.ts
+++ src/lib/authUtils.ts
@@ -1,18 +1,14 @@
 "use client";
 
-import prisma from "../../prisma/prisma";
-
 export const getUserId = async () => {
-//   const { user } = useKindeBrowserClient();
-
-//   const userId = await prisma.user.findUnique({
-//     where: {
-//       kindeId: user.id,
-//     },
-//     select: {
-//       id: true,
-//     },
-//   });
-
-//   return userId?.id;
+  //   const { user } = useKindeBrowserClient();
+  //   const userId = await prisma.user.findUnique({
+  //     where: {
+  //       kindeId: user.id,
+  //     },
+  //     select: {
+  //       id: true,
+  //     },
+  //   });
+  //   return userId?.id;
 };
